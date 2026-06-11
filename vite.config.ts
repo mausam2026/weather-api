2e826ef6e5b435df845c4dd4b697dbb28dc240f1
Mausam Barman <mausambarman711@gmail.com>
2026-06-11 14:57:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://github.com/mausam2026/weather-api", // 👈 MUST match your GitHub repo name
+  base: "/weather-api/",
   plugins: [react()],
 });
